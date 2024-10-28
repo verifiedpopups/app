58bcf753c669b73b501c284d48e5aca6ffb964ec
mattahl <hello@verifiedpopups.com>
2024-10-28 15:49:49 +1000

    first commit

---
+++ app/auth/actions.ts
@@ -0,0 +1,129 @@
+"use server"
+import { createClient } from '@/utils/supabase/server'
+import { redirect } from "next/navigation"
+import { revalidatePath } from 'next/cache'
+import { createStripeCustomer } from '@/utils/stripe/api'
+import { db } from '@/utils/db/db'
+import { usersTable } from '@/utils/db/schema'
+const PUBLIC_URL = process.env.NEXT_PUBLIC_WEBSITE_URL ? process.env.NEXT_PUBLIC_WEBSITE_URL : "http://localhost:3000"
+export async function resetPassword(currentState: { message: string }, formData: FormData) {
+
+    const supabase = createClient()
+    const passwordData = {
+        password: formData.get('password') as string,
+        confirm_password: formData.get('confirm_password') as string,
+        code: formData.get('code') as string
+    }
+    if (passwordData.password !== passwordData.confirm_password) {
+        return { message: "Passwords do not match" }
+    }
+
+    const { data } = await supabase.auth.exchangeCodeForSession(passwordData.code)
+
+    let { error } = await supabase.auth.updateUser({
+        password: passwordData.password
+
+    })
+    if (error) {
+        return { message: error.message }
+    }
+    redirect(`/forgot-password/reset/success`)
+}
+
+export async function forgotPassword(currentState: { message: string }, formData: FormData) {
+
+    const supabase = createClient()
+    const email = formData.get('email') as string
+    const { data, error } = await supabase.auth.resetPasswordForEmail(email, { redirectTo: `${PUBLIC_URL}/forgot-password/reset` })
+
+    if (error) {
+        return { message: error.message }
+    }
+    redirect(`/forgot-password/success`)
+
+}
+export async function signup(currentState: { message: string }, formData: FormData) {
+    const supabase = createClient()
+
+    const data = {
+        email: formData.get('email') as string,
+        password: formData.get('password') as string,
+    }
+
+    const { error } = await supabase.auth.signUp(data)
+
+    if (error) {
+        return { message: error.message }
+    }
+
+    if (error) {
+        redirect('/error')
+    }
+
+    const {
+        data: { user },
+    } = await supabase.auth.getUser()
+    // create Stripe Customer Record
+    const stripeID = await createStripeCustomer(user!.id, user!.email!, "")
+    // Create record in DB
+    await db.insert(usersTable).values({ name: "", email: user!.email!, stripe_id: stripeID, plan: 'none' })
+
+    revalidatePath('/', 'layout')
+    redirect('/subscribe')
+}
+
+export async function loginUser(currentState: { message: string }, formData: FormData) {
+    const supabase = createClient()
+
+    const data = {
+        email: formData.get('email') as string,
+        password: formData.get('password') as string,
+    }
+
+    const { error } = await supabase.auth.signInWithPassword(data)
+
+    if (error) {
+        return { message: error.message }
+    }
+
+    revalidatePath('/', 'layout')
+    redirect('/dashboard')
+}
+
+
+
+export async function logout() {
+    const supabase = createClient()
+    const { error } = await supabase.auth.signOut()
+    redirect('/login')
+}
+
+export async function signInWithGoogle() {
+    const supabase = createClient()
+    const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: 'google',
+        options: {
+            redirectTo: `${PUBLIC_URL}/auth/callback`,
+        },
+    })
+
+    if (data.url) {
+        redirect(data.url) // use the redirect API for your server framework
+    }
+}
+
+
+export async function signInWithGithub() {
+    const supabase = createClient()
+    const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: 'github',
+        options: {
+            redirectTo: `${PUBLIC_URL}/auth/callback`,
+        },
+    })
+
+    if (data.url) {
+        redirect(data.url) // use the redirect API for your server framework
+    }
+
+}
